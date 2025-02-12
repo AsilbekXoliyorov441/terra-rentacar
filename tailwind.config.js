654ad659cb4cd08671f9678a32f80ffe0c24c8a2
Alyorbekjonxon <alyorbek391@gmail.com>
2025-02-12 15:44:25 +0500

    cars-card degan koponenta yaratdim me: Alyorbek

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,16 @@ module.exports = {
   theme: {
     extend: {
       container: {
-        center: true, // Centers the container by default
-        padding: "1rem", // Default padding for the container
+        center: true, 
+        padding: "1rem", 
       },
       screens: {
-        // Custom max-widths for the container
         sm: "640px",
         md: "768px",
         lg: "1024px",
         xl: "1462px",
         "2xl": "1536px",
-        custom: "1800px", // Add a custom breakpoint
+        custom: "1800px", 
       },
     },
   },
