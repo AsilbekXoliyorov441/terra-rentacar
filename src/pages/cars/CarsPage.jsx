597724d1c19395b343f37284321b68dd8e6819d3
Alyorbekjonxon <alyorbek391@gmail.com>
2025-02-12 15:44:25 +0500

    cars-card degan koponenta yaratdim me: Alyorbek

--- src/pages/cars/CarsPage.jsx
+++ src/pages/cars/CarsPage.jsx
@@ -3,9 +3,8 @@ import { Link } from 'react-router-dom';
 
 const CarsPage = () => {
   return (
-    <div>
-      CarsPage
-      <Link className='text-blue-700' to={`/cars/1`}>CarLink</Link>
+    <div className='container w-full  min-h-[680px] border-red m-auto ' >
+      {/* <Link className='text-blue-700' to={`/cars/1`}>CarLink</Link> */}
     </div>
   );
 }
