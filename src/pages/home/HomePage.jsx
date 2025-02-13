67417fc2b4af4bc9cd3a4209f3293c2ecbb39c76
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2025-02-13 09:28:26 +0500

    change bg homePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div className="bg-gray-900 h-[100vh]">HomePage</div>;
+  return <div className="bg-gray-800 h-[100vh]">HomePage</div>;
 }
 
 export default HomePage
