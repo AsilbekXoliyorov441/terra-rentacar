12337e1b4f898f61382b8a8dd41334de9408be39
abdumutal <abdumutalmahamatqulov6@gmail.com>
2025-02-15 12:57:19 +0500

    Added uslugs

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import dc from '../../assets/mm34-DCy6GvLS.png';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <section>
+      <div className='relative m-auto'>
+        <img src={dc} alt="" className=' w-full h-full absolute opacity-30 top-0' />
+        <div className='container relative text-white text-center sm:text-left py-[25px] sm:py-[50px]'>
+          <a href="/" class="text-[#fff9]">Luxury Cars for Rent in Dubai / aboutUs</a>
+          <h3 class="text-[26px] sm:text-[32px] font-semibold my-[15px] sm:my-[50px]">ABOUT US</h3>
+          <h4 class="text-[22px] sm:text-[30px]">Welcome to Auto Zoom Car Rental Co LLC – Your Partner in Unforgettable Journeys in Dubai!</h4>
+          <p class="text-[#cacaca] text-[19px] sm:mb-[50px] mt-[15px]">In a world where every trip is an adventure, Auto Zoom Car Rental Co LLC offers you a unique car rental experience in the heart of Dubai.</p>
+          <h3 class="text-[26px] sm:text-[32px] font-semibold mt-[20px] sm:mt-[50px] mb-[30px]">Why Auto Zoom?</h3>
+          <article class="flex flex-col gap-7">
+            <div class="txt-con leading-[30px]">
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Elegant Car Fleet:</h3>
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">We provide you the opportunity to choose from our luxurious car fleet – from sleek sedans to powerful SUVs. We have a vehicle to match your style and meet your needs.
+              </p>
+            </div>
+            <div class="txt-con leading-[30px]">
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Worry-Free Rental:
+              </h3>
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">Our goal is to make your journey as comfortable as possible. Flexible rental terms, the option to hire with a driver, and a wide range of additional services – all for your convenience.
+              </p>
+            </div>
+            <div class="txt-con leading-[30px]">
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Transparency and Trust:
+              </h3>
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">We value every customer, so our prices are honest and transparent. No hidden fees, no unexpected surprises. Trust us, and we will make your rental experience straightforward and clear.
+              </p>
+            </div>
+            <div class="txt-con leading-[30px]">
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Service at Your Level:
+              </h3>
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">Our friendly and professional team is ready to bring your ideas to life. We aim not just to provide a car but to create a genuine automotive experience for you.
+              </p>
+            </div>
+          </article>
+          <h5 class="text-[22px] font-bold mt-[30px] sm:mt-[100px]">Our Mission:</h5>
+          <p class="mt-2 text-[18px]">Auto Zoom Car Rental Co LLC is created with one goal – to make every journey unforgettable. We strive to offer you not just a car but the key to new experiences and adventures.</p>
+          <h5 class="text-[22px] font-bold mt-[15px]">Contact Us:</h5>
+          <p class="mt-2 text-[18px]">Trust the professionals – trust Auto Zoom Car Rental Co LLC. Get in touch with us today to start your exciting journey through Dubai in our stylish and reliable cars. Allow yourself to savor every moment of your trip.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/services/ServicePage.jsx
+++ src/pages/services/ServicePage.jsx
@@ -16,7 +16,7 @@ const ServicePage = () => {
               <img src={maped} alt="" className='mx-auto md:mx-0' />
               <h4 className='text-white text-[24px] my-[25px] font-semibold leading-[100%]'>Ride a buggy in Dubai and have fun</h4>
               <p className='text-white text-[18px] leading-[100%] mb-[40px]'>Experience the thrill of a dune buggy tour in Dubai with us. We offer free hotel pick-up &amp; drop-off service. Book now</p>
-              <a href="/services" className=' flex items-center justify-evenly w-[200px] hover:ml-4 transition-all duration-700'>
+              <a href="/services/uslugbuggies" className=' flex items-center justify-evenly w-[200px] hover:ml-4 transition-all duration-700'>
                 <p className='text-white font-bold'>LEARN MORE</p>
                 <img data-v-0347924f="" src="data:image/svg+xml,%3csvg%20width='32'%20height='32'%20viewBox='0%200%2032%2032'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20d='M13%2010L21%2016.5L13%2023L17.5714%2016.5L13%2010Z'%20fill='white'%3e%3c/path%3e%3ccircle%20cx='16'%20cy='16'%20r='15.5'%20stroke='white'%3e%3c/circle%3e%3c/svg%3e" alt="" />
               </a>
@@ -27,7 +27,7 @@ const ServicePage = () => {
               <p className='text-white text-[18px] leading-[100%] mb-[40px]'>
                 Professional car photoshoot as an additional type of service at Terra Car Rental
               </p>
-              <a href="" className='flex items-center justify-evenly w-[200px] hover:ml-4 transition-all duration-700'>
+              <a href="/services/uslugcar" className='flex items-center justify-evenly w-[200px] hover:ml-4 transition-all duration-700'>
                 <p className='text-white font-bold'>LEARN MORE</p>
                 <img src="data:image/svg+xml,%3csvg%20width='32'%20height='32'%20viewBox='0%200%2032%2032'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20d='M13%2010L21%2016.5L13%2023L17.5714%2016.5L13%2010Z'%20fill='white'%3e%3c/path%3e%3ccircle%20cx='16'%20cy='16'%20r='15.5'%20stroke='white'%3e%3c/circle%3e%3c/svg%3e" alt="" />
               </a>
