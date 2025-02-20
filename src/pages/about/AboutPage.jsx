8d0c8ef205df8807525f8fe2906be815e4dc9626
abdumutal <abdumutalmahamatqulov6@gmail.com>
2025-02-20 13:36:55 +0500

    language added in data

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,46 +1,56 @@
 import React from 'react';
 import dc from '../../assets/mm34-DCy6GvLS.png';
+import { useSelector } from 'react-redux';
+import { translations } from '../../data';
 
 const AboutPage = () => {
+  const language = useSelector((state) => state.language.language);
   return (
     <section className=''>
       <div className='relative m-auto'>
         <img src={dc} alt="" className='lg:flex w-full h-full absolute opacity-30 top-0' />
         <div className='container m-auto relative text-white text-center sm:text-left py-[25px] sm:py-[50px] px-2'>
-          <a href="/" class="text-[#fff9]">Luxury Cars for Rent in Dubai / aboutUs</a>
-          <h3 class="text-[26px] sm:text-[32px] font-semibold my-[15px] sm:my-[50px]">ABOUT US</h3>
-          <h4 class="text-[22px] sm:text-[30px]">Welcome to Auto Zoom Car Rental Co LLC – Your Partner in Unforgettable Journeys in Dubai!</h4>
-          <p class="text-[#cacaca] text-[19px] sm:mb-[50px] mt-[15px]">In a world where every trip is an adventure, Auto Zoom Car Rental Co LLC offers you a unique car rental experience in the heart of Dubai.</p>
-          <h3 class="text-[26px] sm:text-[32px] font-semibold mt-[20px] sm:mt-[50px] mb-[30px]">Why Auto Zoom?</h3>
+          <div className='flex justify-start'>
+            <a href="/">
+              <p class="text-[#fff9] text-[14px] text-center md:text-left">
+                {translations[language]?.breadcrumb1 || translations?.en?.breadcrumb1}/
+              </p>
+            </a>
+            <a href="/about" className='text-[#fff9] text-[14px] text-center md:text-left'>{translations[language]?.aboutUs || translations?.en.aboutUs}</a>
+          </div>
+          <h3 class="text-[26px] sm:text-[32px] font-semibold my-[15px] sm:my-[50px]">{translations[language]?.aboutUs || translations?.en.aboutUs}</h3>
+          <h4 class="text-[22px] sm:text-[30px]">{translations[language]?.autozoom || translations?.en.autozoom}</h4>
+          <p class="text-[#cacaca] text-[19px] sm:mb-[50px] mt-[15px]">{translations[language]?.autozoom1 || translations?.en.autozoom1}</p>
+          <h3 class="text-[26px] sm:text-[32px] font-semibold mt-[20px] sm:mt-[50px] mb-[30px]">{translations[language]?.autozoom2 || translations?.en.autozoom2}</h3>
           <article class="flex flex-col gap-7">
             <div class="txt-con leading-[30px]">
-              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Elegant Car Fleet:</h3>
-              <p class="inline text-[#d8d8d8] text-[18px] font-normal">We provide you the opportunity to choose from our luxurious car fleet – from sleek sedans to powerful SUVs. We have a vehicle to match your style and meet your needs.
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">{translations[language]?.elegant || translations?.en.elegant}</h3>
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">{translations[language]?.elegant1 || translations?.en.elegant1}
               </p>
             </div>
             <div class="txt-con leading-[30px]">
-              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Worry-Free Rental:
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">{translations[language]?.rental || translations?.en.rental}
               </h3>
-              <p class="inline text-[#d8d8d8] text-[18px] font-normal">Our goal is to make your journey as comfortable as possible. Flexible rental terms, the option to hire with a driver, and a wide range of additional services – all for your convenience.
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">{translations[language]?.rental1 || translations?.en.rental1}
               </p>
             </div>
             <div class="txt-con leading-[30px]">
-              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Transparency and Trust:
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">{translations[language]?.trans || translations?.en.trans}
               </h3>
-              <p class="inline text-[#d8d8d8] text-[18px] font-normal">We value every customer, so our prices are honest and transparent. No hidden fees, no unexpected surprises. Trust us, and we will make your rental experience straightforward and clear.
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">{translations[language]?.costum || translations?.en.costum}
               </p>
             </div>
             <div class="txt-con leading-[30px]">
-              <h3 class="text-[22px] font-bold inline-block mr-[5px]">Service at Your Level:
+              <h3 class="text-[22px] font-bold inline-block mr-[5px]">{translations[language]?.level || translations?.en.level}
               </h3>
-              <p class="inline text-[#d8d8d8] text-[18px] font-normal">Our friendly and professional team is ready to bring your ideas to life. We aim not just to provide a car but to create a genuine automotive experience for you.
+              <p class="inline text-[#d8d8d8] text-[18px] font-normal">{translations[language]?.professional || translations?.en.professional}
               </p>
             </div>
           </article>
-          <h5 class="text-[22px] font-bold mt-[30px] sm:mt-[100px]">Our Mission:</h5>
-          <p class="mt-2 text-[18px]">Auto Zoom Car Rental Co LLC is created with one goal – to make every journey unforgettable. We strive to offer you not just a car but the key to new experiences and adventures.</p>
-          <h5 class="text-[22px] font-bold mt-[15px]">Contact Us:</h5>
-          <p class="mt-2 text-[18px]">Trust the professionals – trust Auto Zoom Car Rental Co LLC. Get in touch with us today to start your exciting journey through Dubai in our stylish and reliable cars. Allow yourself to savor every moment of your trip.</p>
+          <h5 class="text-[22px] font-bold mt-[30px] sm:mt-[100px]">{translations[language]?.mission || translations?.en.mission}</h5>
+          <p class="mt-2 text-[18px]">{translations[language]?.zoomrental || translations?.en.zoomrental}</p>
+          <h5 class="text-[22px] font-bold mt-[15px]">{translations[language]?.contactUs || translations?.en.contactUs}</h5>
+          <p class="mt-2 text-[18px]">{translations[language]?.zoomcar || translations?.en.zoomcar}</p>
         </div>
       </div>
     </section>
--- src/pages/uslug-car/UslugCarPage.jsx
+++ src/pages/uslug-car/UslugCarPage.jsx
@@ -3,59 +3,68 @@ import sportcar from '../../assets/uslug-sportcar-1-Ck5Sy84C.jpg';
 import sportcat2 from '../../assets/uslug-sportcar-2-eD2_lVq8.jpg';
 import sportcat3 from '../../assets/uslug-sportcar-3-CI-yhwgq.jpg';
 import dowloud from '../../assets/download.png';
+import { useSelector } from 'react-redux';
+import { translations } from '../../data';
 
 const UslugCarPage = () => {
-
+  const language = useSelector((state) => state.language.language);
 
   return (
     <section className='bg-[#1e1f27]'>
 
       <div className='container  py-[50px] m-auto'>
-        <p class="text-[#fff9] text-[14px] text-center md:text-left mx-2">
-          Luxury Cars for Rent in Dubai/ Services/ Photoshoot with luxury car rental Dubai
-        </p>
+        <div className='flex justify-start'>
+
+          <a href="/">
+            <p class="text-[#fff9] text-[14px] text-center md:text-left">
+              {translations[language]?.breadcrumb1 || translations?.en?.breadcrumb1}/
+            </p>
+          </a>
+          <a href="/services" className='text-[#fff9] text-[14px] text-center md:text-left'> {translations[language]?.breadcrumb2 || translations?.en?.breadcrumb2}/</a>
+          <a href="uslugcar" className='text-[#fff9] text-[14px] text-center md:text-left'>{translations[language]?.service2Title || translations?.en?.service2Title}</a>
+        </div>
         <h3 class="text-white text-[32px] text-center md:text-left my-[10px] p768:my-[50px] mx-2 font-semibold">
-          Photoshoot with luxury car rental Dubai
+          {translations[language]?.service2Title || translations?.en?.service2Title}
         </h3>
         <div className='flex gap-2 flex-wrap justify-center  p-2 py-[50px] lg:justify-between'>
           <div className='w-[300px] md:w-[380px] h-full bg-gradient-to-br from-[#29292944] via-[#29292944] to-[#95979727] border-[#e5e7eb] rounded-[10px]  transition-all duration-1000'>
             <img src={sportcar} alt="" className='w-full h-[230px] rounded-lg' />
             <div className='text-container px-[25px]'>
-              <h3 className='text-white mt-[25px]'>BASIC</h3>
+              <h3 className='text-white mt-[25px]'>{translations[language]?.easy || translations?.en.easy} </h3>
               <div className='text-white mt-[30px] flex items-center gap-5'>
                 <p className='border border-solid text-[18px] border-[#11d198] py-1 px-5 rounded-lg'>1000</p>
-                <p>one person</p>
+                <p>{translations[language]?.person1 || translations?.en.person1}</p>
               </div>
               <p className='text-white mt-[25px]'>
-                Driving a sports car and a premium photo shoot in Dubai. Book yourself a trip to the most exciting adventure in the desert, and then enjoy the traditional Arab hospitality
+                {translations[language]?.serviceDescription || translations?.en.serviceDescription}
               </p>
-              <p className='text-white text-[26px] font-semibold my-[25px]'>Package Inclusions</p>
+              <p className='text-white text-[26px] font-semibold my-[25px]'>{translations[language]?.package || translations?.en.package}</p>
               <div className='flex items-center gap-5 mt-[15px]'>
                 <img src={dowloud} alt="" />
-                <p className='text-white text-[18px] font-semibold'>Premium Transfer to camp</p>
+                <p className='text-white text-[18px] font-semibold'>{translations[language]?.premium || translations?.en.premium}</p>
               </div>
               <div className='flex items-center gap-5 mt-[15px]'>
                 <img src={dowloud} alt="" />
-                <p className='text-white text-[18px] font-semibold'>Dune Buggy riding 1.5 hours</p>
+                <p className='text-white text-[18px] font-semibold'>{translations[language]?.dune || translations?.en.dune}</p>
               </div>
               <div className='flex items-center gap-5 mt-[15px]'>
                 <img src={dowloud} alt="" />
-                <p className='text-white text-[18px] font-semibold'>Falcone shoting</p>
+                <p className='text-white text-[18px] font-semibold'>{translations[language]?.falcone || translations?.en.falcone}</p>
               </div>
               <div className='flex items-center gap-5 mt-[15px]'>
                 <img src={dowloud} alt="" />
-                <p className='text-white text-[18px] font-semibold'>Camel</p>
+                <p className='text-white text-[18px] font-semibold'>{translations[language]?.camel || translations?.en.camel}</p>
               </div>
               <div className='flex items-center gap-5 mt-[15px]'>
                 <img src={dowloud} alt="" />
-                <p className='text-white text-[18px] font-semibold'>VIP room</p>
+                <p className='text-white text-[18px] font-semibold'>{translations[language]?.vip || translations?.en.vip}</p>
               </div>
               <div className='flex items-center gap-5 mt-[15px]'>
                 <img src={dowloud} alt="" />
-                <p className='text-white text-[18px] font-semibold'>Fruits, Drinks, BBQ</p>
+                <p className='text-white text-[18px] font-semibold'>{translations[language]?.fruit || translations?.en.fruit}</p>
               </div>
               <a href="tel:+998990000441">
-                <button className='w-full bg-[#fe363b] mt-[15px] text-white py-3 font-bold text-[26px] rounded-2xl'>BOOK NOW</button>
+                <button className='w-full bg-[#fe363b] mt-[15px] text-white py-3 font-bold text-[26px] rounded-2xl'>{translations[language]?.book || translations?.en.book}</button>
               </a>
             </div>
           </div>
@@ -63,41 +72,41 @@ const UslugCarPage = () => {
             <div className='w-[300px] md:w-[380px] h-full bg-gradient-to-br from-[#29292944] via-[#29292944] to-[#95979727] border-[#e5e7eb] rounded-[10px]  transition-all duration-1000'>
               <img src={sportcat2} alt="" className='w-full h-[230px] rounded-lg' />
               <div className='text-container px-[25px]'>
-                <h3 className='text-white mt-[25px]'>BASIC</h3>
+                <h3 className='text-white mt-[25px]'>{translations[language]?.easy || translations?.en.easy} </h3>
                 <div className='text-white mt-[30px] flex items-center gap-5'>
                   <p className='border border-solid text-[18px] border-[#11d198] py-1 px-5 rounded-lg'>2000</p>
-                  <p>two person</p>
+                  <p>{translations[language]?.person2 || translations?.en.person2}</p>
                 </div>
                 <p className='text-white mt-[25px]'>
-                  Driving a sports car and a premium photo shoot in Dubai. Book yourself a trip to the most exciting adventure in the desert, and then enjoy the traditional Arab hospitality
+                  {translations[language]?.serviceDescription || translations?.en.serviceDescription}
                 </p>
-                <p className='text-white text-[26px] font-semibold my-[25px]'>Package Inclusions</p>
+                <p className='text-white text-[26px] font-semibold my-[25px]'>{translations[language]?.package || translations?.en.package}</p>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Premium Transfer to camp</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.premium || translations?.en.premium}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Dune Buggy riding 1.5 hours</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.dune || translations?.en.dune}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Falcone shoting</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.falcone || translations?.en.falcone}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Camel</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.camel || translations?.en.camel}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>VIP room</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.vip || translations?.en.vip}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Fruits, Drinks, BBQ</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.fruit || translations?.en.fruit}</p>
                 </div>
                 <a href="tel:+998990000441">
-                  <button className='w-full bg-[#fe363b] mt-[15px] text-white py-3 font-bold text-[26px] rounded-2xl'>BOOK NOW</button>
+                  <button className='w-full bg-[#fe363b] mt-[15px] text-white py-3 font-bold text-[26px] rounded-2xl'>{translations[language]?.book || translations?.en.book}</button>
                 </a>
               </div>
             </div>
@@ -106,41 +115,41 @@ const UslugCarPage = () => {
             <div className='w-[300px] md:w-[380px] h-full bg-gradient-to-br from-[#29292944] via-[#29292944] to-[#95979727] border-[#e5e7eb] rounded-[10px]  transition-all duration-1000'>
               <img src={sportcat3} alt="" className='w-full h-[230px] rounded-lg' />
               <div className='text-container px-[25px]'>
-                <h3 className='text-white mt-[25px]'>BASIC</h3>
+                <h3 className='text-white mt-[25px]'>{translations[language]?.easy || translations?.en.easy} </h3>
                 <div className='text-white mt-[30px] flex items-center gap-5'>
                   <p className='border border-solid text-[18px] border-[#11d198] py-1 px-5 rounded-lg'>3  000</p>
-                  <p>three person</p>
+                  <p>{translations[language]?.person3 || translations?.en.person3}</p>
                 </div>
                 <p className='text-white mt-[25px]'>
-                  Driving a sports car and a premium photo shoot in Dubai. Book yourself a trip to the most exciting adventure in the desert, and then enjoy the traditional Arab hospitality
+                  {translations[language]?.serviceDescription || translations?.en.serviceDescription}
                 </p>
-                <p className='text-white text-[26px] font-semibold my-[25px]'>Package Inclusions</p>
+                <p className='text-white text-[26px] font-semibold my-[25px]'>{translations[language]?.package || translations?.en.package}</p>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Premium Transfer to camp</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.premium || translations?.en.premium}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Dune Buggy riding 1.5 hours</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.dune || translations?.en.dune}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Falcone shoting</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.falcone || translations?.en.falcone}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Camel</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.camel || translations?.en.camel}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>VIP room</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.vip || translations?.en.vip}</p>
                 </div>
                 <div className='flex items-center gap-5 mt-[15px]'>
                   <img src={dowloud} alt="" />
-                  <p className='text-white text-[18px] font-semibold'>Fruits, Drinks, BBQ</p>
+                  <p className='text-white text-[18px] font-semibold'>{translations[language]?.fruit || translations?.en.fruit}</p>
                 </div>
                 <a href="tel:+998990000441">
-                  <button className='w-full bg-[#fe363b] mt-[15px] text-white py-3 font-bold text-[26px] rounded-2xl'>BOOK NOW</button>
+                  <button className='w-full bg-[#fe363b] mt-[15px] text-white py-3 font-bold text-[26px] rounded-2xl'>{translations[language]?.book || translations?.en.book}</button>
                 </a>
               </div>
             </div>
